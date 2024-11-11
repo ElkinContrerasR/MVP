bbd52e3200c409a7b39cb728d75fdb7179c65957
Elkin Contreras <escontreras-2023a@corhuila.edu.co>
2024-11-11 11:17:06 -0500

    primero

---
+++ GestionContactos/GestionContactos/src/main/java/com/proyecto/GestionContactos/IService/IContactoService.java
@@ -0,0 +1,28 @@
+package com.proyecto.GestionContactos.IService;
+
+import com.proyecto.GestionContactos.Entity.Contacto;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IContactoService {
+
+    //Guardar
+    Contacto save(Contacto contacto);
+
+    //Actualizar
+    void update(Contacto contacto, Integer id);
+
+    //Mostrartodo
+    List<Contacto> all();
+    Optional<Contacto> findById(Integer id);
+
+    //Eliminar
+    void delete(Integer id);
+
+    List<Contacto> findByUsuario(String nombreUsuario);
+
+
+
+};
